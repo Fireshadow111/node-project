222a5627120e279d1ea749860f56cb68d5e09860
Fireshadow111 <muhammadrajah16@gmail.com>
2024-02-27 09:53:44 +0200

    creating server

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import {config} from 'dotenv';
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+import productsRouter from '../backend/routes/products-routes.js';
+import userRouter from '../backend/routes/user-routes.js'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
+config()
+
+const PORT=process.env.MYSQL_ADDON_PORT || 9001
+
+const app=express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.static('views'))
+
+app.use(cookieParser())
+
+app.use('/products', productsRouter)
+
+app.use('/user', userRouter)
+
+app.listen (PORT,()=>{
+    console.log(`this is listening on http://localhost:${PORT}`)
+})
+
+
+
