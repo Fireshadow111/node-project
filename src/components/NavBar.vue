8345104a83f227fa7f13a66bf14cf245f0c70bdf
NatashaKapeyi <natashazikhona7@gmail.com>
2024-03-01 16:14:54 +0200

    done

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -38,5 +38,11 @@ export default {
 }
 </script>
   
-<style ></style>
+<style scoped>
+ @media screen and (max-width: 900px){
+  .table{
+    width: fit-content;
+  }
+ }
+</style>
   
--- src/store/index.js
+++ src/store/index.js
@@ -33,31 +33,51 @@ export default createStore({
       try {
         const {data} = await axios.get(baseUrl+'/products');
         commit("setProducts", data);
+        
       } catch (error) {
         console.error('Error fetching products:', error);
       }
     },
 
     async getProduct({commit},prod_ID){
-      const {data} = await axios.get(baseUrl+'/products/'+prod_ID)
+      try{const {data} = await axios.get(baseUrl+'/products/'+prod_ID)
       commit("setProduct", data);
+    }catch(error){
+      console.error('Error fetching product:', error);
+    }
+      
     },
 
     async addProduct({commit},add) {
-     const {data} = await axios.post(baseUrl+'/products',add)
+      try {
+         const {data} = await axios.post(baseUrl+'/products',add)
      commit("setProducts", data);
      window.location.reload();
+      } catch (error) {
+        console.error('Error adding product:', error);
+      }
+    
      
     },
 
     async deleteProd({commit},prod_ID){
-       await axios.delete(baseUrl+'/products/'+prod_ID)
+      try {
+        await axios.delete(baseUrl+'/products/'+prod_ID)
       window.location.reload();
+      } catch (error) {
+        console.error('Error deleting product:', error);
+      }
+       
     },
 
     async editProd({commit},update){
-      await axios.patch(baseUrl+'/products/'+update.id,update)
+      try {
+         await axios.patch(baseUrl+'/products/'+update.id,update)
       window.location.reload();
+      } catch (error) {
+        console.error('Erro editing product:', error);
+      }
+     
     },
 
 
@@ -71,47 +91,79 @@ export default createStore({
       }
     },
     async getUser({commit}){
-      await axios.get(baseUrl+'/users')
+      try {
+        await axios.get(baseUrl+'/users')
+      } catch (error) {
+        console.error('Error getting user:', error);
+      }
+      
     },
     async addUser({commit},add) {
-     const {data} = await axios.post(baseUrl+'/users',add)
+      try {
+         const {data} = await axios.post(baseUrl+'/users',add)
      commit("setUser", data);
      window.location.reload();
+      } catch (error) {
+        console.error('Error adding user:', error);
+      }
+    
     },
     async deleteUser({commit},user_ID){
-       await axios.delete(baseUrl+'/users/'+user_ID)
+      try {
+          await axios.delete(baseUrl+'/users/'+user_ID)
       window.location.reload();
+      } catch (error) {
+        console.error('Error deleting user:', error);
+      }
+     
     },
     async editUser({commit},update){
-      await axios.patch(baseUrl+'/users/'+update.id,update)
-      window.location.reload();
+      try {
+       await axios.patch(baseUrl+'/users/'+update.id,update)
+      window.location.reload();  
+      } catch (error) {
+        console.error('Error edit user:', error);
+      }
+     
     },
 
-
-
-
     async registerUser({commit}, newUser){
-      console.log(newUser);
+      try {
+         console.log(newUser);
       let {data} = await axios.post(baseUrl + '/users', newUser)
       alert(data.msg)
       window.location.reload()
+      } catch (error) {
+        console.error('Error registering product:', error);
+      }
+     
     },
     async loginUser({commit}, currentUser){
-      let {data} = await axios.post(baseUrl + '/login', currentUser)
+      try {
+          let {data} = await axios.post(baseUrl + '/login', currentUser)
       $cookies.set('jwt', data.token)
       alert(data.msg)
       router.push('/')
       window.location.reload()
       commit('setLogged', true)
       window.location.reload()
+      } catch (error) {
+        console.error('Error login user:', error);
+      }
+    
     },
     async logOut(context){
-      let cookies = $cookies.keys()
+      try {
+           let cookies = $cookies.keys()
       console.log(cookies)
       $cookies.remove('jwt')
       window.location.reload()
       let { data } = await axios.delete(baseUrl + '/logout')
       alert(data.msg)
+      } catch (error) {
+        console.error('Error logging user:', error);
+      }
+   
     }
 
 
